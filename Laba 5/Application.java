d757784bebe29691dae992fdcbcab3955d5a8515
Ostap1963 <34818509+Ostap1963@users.noreply.github.com>
2018-03-22 23:21:18 +0200

    Add files via upload

---
+++ Laba 5/Application.java
@@ -0,0 +1,33 @@
+public class Application {
+
+    public static void main(String args[]) {
+
+        if(args == null || args.length <= 0 || args.length > 2 || args[0] == null) {
+            System.out.println("Error");
+        }
+        else if(args[0].equals("help") && args.length == 1) {
+            HelpCommand command = new HelpCommand();
+            command.execute();
+        }
+        else if(args[0].equals("exit") && args.length == 1) {
+            ExitCommand command = new ExitCommand();
+            command.execute();
+        }
+        else if(args[0].equals("date") && args[1].equals("now") && args.length < 3) {
+            DateCommand command = new DateCommand();
+            command.execute();
+        }
+        else if(args[0].equals("echo") && args[1] != null && args.length < 3) {
+            EchoCommand command = new EchoCommand(args[1]);
+            command.execute();
+        }
+        else {
+            System.out.println("Error");
+        }
+    }
+}
+//if(args == null || args[0] == null  || args.length > 2 ||
+//        !args[0].equals("help") && !args[0].equals("exit") && !args[0].equals("echo") && !args[0].equals("date")) {
+//        System.out.println("Error");
+//        }
+//        else
